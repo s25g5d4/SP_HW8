4479d4e50490134c10c8aa7626769eb0719962fd
s25g5d4 <s25g5d4@gmail.com>
2015-12-08 20:48:30 +0800

    main function

--- sms_server.c
+++ sms_server.c
@@ -9,6 +9,47 @@
 
 #include "sms.h"
 
+Servlet *door;			/* entrance to doubly linked list		*/
+Pending *pending_stack;	/* pile of stale servlets				*/
+Stats * history;			/* start of stats linked list			*/
+sem_t garbage_time;		/* schedule garbage collection			*/
+pthread_attr_t attr;		/* set to make ALL threads detached		*/
+
+char chats[4][64] = {
+	"1) Display number of current connections",
+	"2) Display statistics (average connect time etc.)",
+	"3) Re-start statistics gathering",
+	"4) Kill stale clients"
+};
+
 void main(int argc, char **argv) {
-	
+	Menu_item items[4];
+	items[0].chat = chats[0];
+	items[0].act = list_conn;
+	items[1].chat = chats[1];
+	items[1].act = list_stats;
+	items[2].chat = chats[2];
+	items[2].act = zero_stats;
+	chats[3].chat = chats[3];
+	items[3].act = zap_stale;
+
+	Menu_item *menus[5] = {
+		items,
+		items + 1,
+		items + 2,
+		items + 3,
+		NULL
+	};
+
+	pthread_attr_init(&attr);
+	sem_init(&garbage_time, 0, 0);
+
+	pthread_t garbage, listen;
+
+	pthread_create(&garbage, &attr, collect_garb, NULL);
+	pthread_create(&listen, &attr, listen_port, (void *)PORT);
+
+	menu(menus);
+
+	return 0;
 }
